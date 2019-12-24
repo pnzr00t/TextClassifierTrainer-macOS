e08d45c1876502fef0598cee1cb559906e74938e
pnzr00t <pnz.r00t@gmail.com>
2019-12-25 02:18:54 +0300

    1	New amazon instant video review with overall score (1.0 - 5.0 AS STRING). 37000+ reviews 		IMPORTANT: Overall as string (Restriction of CoreML lib) 2 	Split json array Training set - 70%, Testing set 30% 3 	Training accuracy -  99.16777000189143. Evaludate accuracy -  57.721447946871194 4 	Iteration 44 training accuracy 0.991867

--- TextClassifierTrainer.playground/Contents.swift
+++ TextClassifierTrainer.playground/Contents.swift
@@ -2,10 +2,89 @@
 import Foundation
 import CreateML
 
-guard let trainingDataFileURL = Bundle.main.url(forResource: "amazon-reviews", withExtension: "json"),
+
+
+func readFileAndSplitByFile(baseFileDataUrl: URL, trainingDataFileURL: URL, testingDataFileURL: URL) -> Bool {
+    do { // clear files with data tranin and test data
+        try "".write(to: trainingDataFileURL, atomically: true, encoding: .utf8)
+        try "".write(to: testingDataFileURL, atomically: true, encoding: .utf8)
+    } catch {
+        fatalError("Cant clear train and test file \(error.localizedDescription)" )
+    }
+    
+    guard let trainingFileHandle = try? FileHandle(forWritingTo: trainingDataFileURL),
+    let testingFileHandle = try? FileHandle(forWritingTo: testingDataFileURL) else {
+        fatalError("Error! Could not load resource files.")
+    }
+    
+    testingFileHandle.write(("[").data(using: .utf8)!)
+    trainingFileHandle.write(("[").data(using: .utf8)!)
+
+    
+    if freopen(baseFileDataUrl.path, "r", stdin) == nil {
+        perror(baseFileDataUrl.path)
+        return false
+    }
+    
+    var testingLineCount: Int = 0
+    var trainingLineCount: Int = 0
+    
+    while let line = readLine() {
+        print(line)
+        
+        if Int.random(in: 0..<100) > 70 {
+            testingFileHandle.seekToEndOfFile()
+            if testingLineCount > 0 {
+                testingFileHandle.write(("," + "\n" + line).data(using: .utf8)!)
+            } else {
+                testingFileHandle.write(("\n" + line).data(using: .utf8)!)
+            }
+                
+            print("Testing data: ", line)
+            
+            testingLineCount += 1
+        } else {
+            trainingFileHandle.seekToEndOfFile()
+            if trainingLineCount > 0 {
+                trainingFileHandle.write(("," + "\n" + line).data(using: .utf8)!)
+            } else {
+                trainingFileHandle.write(("\n" + line).data(using: .utf8)!)
+            }
+            print("Training data: ", line)
+            
+            trainingLineCount += 1
+        }
+    }
+    
+    testingFileHandle.write(("\n]").data(using: .utf8)!)
+    trainingFileHandle.write(("\n]").data(using: .utf8)!)
+    trainingFileHandle.closeFile()
+    testingFileHandle.closeFile()
+    
+    print("trainingDataFileURL = ", trainingDataFileURL)
+    print("testingDataFileURL = ", testingDataFileURL)
+    return true
+}
+
+// get URL to the the documents directory in the sandbox
+let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+let trainingDataFileURL = documentsUrl.appendingPathComponent("training-set.json")
+let testingDataFileURL = documentsUrl.appendingPathComponent("testing.json")
+
+guard let amazonInstatnVideoUrl = Bundle.main.url(forResource: "reviews_Amazon_Instant_Video_5", withExtension: "json") else {
+    fatalError("Cant reda Amazon input file")
+}
+    
+    // Split file by training and Data set
+    readFileAndSplitByFile(baseFileDataUrl: amazonInstatnVideoUrl, trainingDataFileURL: trainingDataFileURL, testingDataFileURL: testingDataFileURL)
+
+
+
+/*guard let trainingDataFileURL = Bundle.main.url(forResource: "amazon-reviews", withExtension: "json"),
 let testingDataFileURL = Bundle.main.url(forResource: "testing-reviews", withExtension: "json") else {
     fatalError("Error! Could not load resource files.")
-}
+}*/
 
 var trainingDataTable: MLDataTable? = nil
 var testingDataTable: MLDataTable? = nil
@@ -29,13 +108,13 @@ do {
     
     
     
-    let mlTextClassifier = try MLTextClassifier(trainingData: trainingDataTable, textColumn: "text", labelColumn: "label")
+    let mlTextClassifier = try MLTextClassifier(trainingData: trainingDataTable, textColumn: "reviewText", labelColumn: "overall")
     
     let trainingAccuracy = (1.0 - mlTextClassifier.trainingMetrics.classificationError) * 100
     let validateAccuracy = (1.0 - mlTextClassifier.validationMetrics.classificationError) * 100
     
     
-    let evaluateMetrics = mlTextClassifier.evaluation(on: testingDataTable, textColumn: "text", labelColumn: "label")
+    let evaluateMetrics = mlTextClassifier.evaluation(on: testingDataTable, textColumn: "reviewText", labelColumn: "overall")
     let evaluateAccuracy = (1.0 - evaluateMetrics.classificationError) * 100
     
     print("Training accuracy - ", trainingAccuracy)
@@ -49,12 +128,18 @@ do {
 
     
     let metaDataForModel = MLModelMetadata(author: "POA",
-                                           shortDescription: "A model trained to classify product review sentiment", version: "0.1")
+                                           shortDescription: "A model trained to classify product review sentiment", version: "0.2")
 
     
-    let modelFileURL = URL(fileURLWithPath: "/Volumes/Samsung_T5/Users/TestFolder/CoreML/NauralLanguage/TextClassifierTrainer/TrainedTextClassifierModel/ReviewClassifier.mlmodel")
-    try mlTextClassifier.write(to: modelFileURL, metadata: metaDataForModel)
+    // get URL to the the documents directory in the sandbox
+    let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
 
+    // add a filename
+    let outModelFileUrl = documentsUrl.appendingPathComponent("ReviewClassifier.mlmodel")
+    
+    //let modelFileURL = URL(fileURLWithPath: "/Volumes/Samsung_T5/Users/TestFolder/CoreML/NauralLanguage/TextClassifierTrainer/TrainedTextClassifierModel/ReviewClassifier.mlmodel")
+    try mlTextClassifier.write(to: outModelFileUrl, metadata: metaDataForModel)
+    print("Model file path = ", outModelFileUrl);
 } catch {
     print("Training info exception - ", error.localizedDescription)
     exit(0)
